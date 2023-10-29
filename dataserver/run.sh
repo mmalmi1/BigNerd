88083348bc9e732a38cf5759397dc4a19fd0a62f
Debian <admin@ip-172-31-40-133.eu-north-1.compute.internal>
2023-10-29 16:35:46 +0000

    Update for settings for ec2

--- dataserver/run.sh
+++ dataserver/run.sh
@@ -1,2 +1,2 @@
 docker build -t dataserver .
-docker run -p 5000:5000 -v $(pwd)/sqlite3:/sqlite3 dataserver
+docker run -d -p 5000:5000 -v $(pwd)/sqlite3:/sqlite3 dataserver
--- frontend/run.sh
+++ frontend/run.sh
@@ -1,2 +1,2 @@
 docker build -t frontend .
-docker run -p 443:443 -p 80:80 --network=host frontend
+docker run -d -p 8754:8754 -p 443:443 -p 80:80 --network=host frontend
